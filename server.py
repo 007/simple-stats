bb87a7117cbe3974d27491fff99df9715b83b1f1
scripting <scripting@f4a64cd6-bb93-48a9-bb03-caeeccae09ce>
2010-10-26 03:18:05 +0000

    progress on stat and summary/dump tasks, mostly need to follow TODOs
    git-svn-id: https://murdock.reputationdefender.com/sourcecode/engineering/logserver/stats@240 f4a64cd6-bb93-48a9-bb03-caeeccae09ce

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os, re
+
 import rrdtool
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
@@ -7,46 +9,87 @@ from twisted.internet import reactor
 pref_port = 13023
 
 
-def create_rrd(fname, dsname):
-    # where to store RRDs
-    RRD_basepath = '/home/rmoore/stats/'
- 
-    # default step size, 10-second resolution
-    RRD_step = 10
+class RRD(object):
+    def clean_str(stat):
+        # create a clean version of a string for RRDs, only alpha-num and underscore
+        return re.sub('[^A-Za-z0-9]+', '_', str(stat))
 
-    # default ranges to keep in our RRD:
-    # 24 hours of 10-second average
-    # 10 days of 1-minute average
-    # 40 days of 1-hour average
-    # 1 year of 1-day average
-    # 10 years of 10-day average
+    def map_name(stat):
+        # map a stat name to a physical RRD file
+        # TODO: create subdirectories based on fname or hash(fname) so we don't dump 10k RRDs in a single dir
+        RRD_basepath = '/home/rmoore/stats/'
+        return RRD_basepath + self.clean_str(stat) + '.rrd'
 
-    # time scale: 1 day, 10 days, 40 days,   1 year, 10 years (in seconds)
-    RRD_scale = [ 86400,  864000, 3456000, 31622400, 315576000 ]
+    # example: create('hits', 'apache_hits') to create hits.rrd containing apache_hits as a data source
+    def create(fname, dsname):
+        # where to store RRDs
+     
+        # default step size, 10-second resolution
+        RRD_step = 10
 
-    # resolution: 10 second, 1 minute, 1 hour, 1 day, 10 day (in seconds) 
-    RRD_res = [          10,       60  , 3600, 86400, 864000 ]
+        # default ranges to keep in our RRD:
+        # 24 hours of 10-second average
+        # 10 days of 1-minute average
+        # 40 days of 1-hour average
+        # 1 year of 1-day average
+        # 10 years of 10-day average
 
-    # explict str cast for fname and dsname, in case they're INT from input
-    RRD_PARAMS = [ RRD_basepath + str(fname) + '.rrd', '--step', str(RRD_step), 'DS:' + str(dsname) + ':GAUGE:12:0:U' ]
-    # TODO: create subdirectories based on fname or hash(fname) so we don't dump 10k RRDs in a single dir
+        # time scale: 1 day, 10 days, 40 days,   1 year, 10 years (in seconds)
+        RRD_scale = [ 86400,  864000, 3456000, 31622400, 315576000 ]
 
-    for res, scale in zip(RRD_res, RRD_scale):
-        step_res = res / RRD_step
-        step_row = scale / res
-        RRD_PARAMS.append('RRA:AVERAGE:0.99999:' + str(step_res) + ':' + str(step_row))
+        # resolution: 10 second, 1 minute, 1 hour, 1 day, 10 day (in seconds) 
+        RRD_res = [          10,       60  , 3600, 86400, 864000 ]
 
-    result = apply(rrdtool.create, RRD_PARAMS)
+        RRD_PARAMS = [ self.map_name(fname), '--step', str(RRD_step), 'DS:' + self.clean_str(dsname) + ':GAUGE:12:0:U' ]
 
+        for res, scale in zip(RRD_res, RRD_scale):
+            step_res = res / RRD_step
+            step_row = scale / res
+            RRD_PARAMS.append('RRA:AVERAGE:0.99999:' + str(step_res) + ':' + str(step_row))
 
-# example: create_rrd('hits', 'apache_hits') to create hits.rrd containing apache_hits as a data source
+        result = apply(rrdtool.create, RRD_PARAMS)
+
+    def update(stat, val):
+        stat_file = self.map_name(stat)
+        if !os.path.isfile(statfile):
+            # need to create file, then we can update
+            self.create(stat, stat)
+        # TODO: fix this
+        rrdtool.update(stat_file, stat, val)
+
+class Stats(object):
+    def update(type, key, val):
+        # TODO: update these to actually work
+        if !type in self.stats:
+            self.stats[type] = {}
+        if !key in self.stats[type]:
+            self.stats[type][key] = 0;
+        # update a stat, internally
+        self.stats[type][key] += val
+
+    def dump():
+        # dump stats to RRDs
+        for stat, val in self.stats
+            RRD.update(stat, val)
+
 
 class StatServer(DatagramProtocol):
     def __init__(self, core):
         """Note: Parent class has no __init__ to call"""
-        self.rollup_task = None
+        self.summarize = None
         self.stats = {}
         self.core = core
+
+    def startProtocol(self):
+        protocol.DatagramProtocol.startProtocol(self)
+
+        # start stat dump
+        self.summarize = task.LoopingCall(self.dumpStats)
+        self.summarize.start(10, now=False)
+
+    def stopProtocol(self):
+        protocol.DatagramProtocol.stopProtocol(self)
+
 
     def updateState(type, key, value):
         print "Updating %s with %s using %s" % (key, value, type)
@@ -57,6 +100,9 @@ class StatServer(DatagramProtocol):
         for line in data.splitlines():
             type, key, val = line.split()
             Stat.update(type, key, val)
+
+    def dumpStats():
+        for statName, statVal in self.stats
 
 
 reactor.listenUDP(pref_port, StatServer())
@@ -74,19 +120,6 @@ reactor.run
  
 class ReflexProtocol(protocol.DatagramProtocol):
 
-    def __init__(self, core):
-        """Note: Parent class has no __init__ to call"""
-        self.rollup_task = None
-        self.stats = {}
-        self.core = core
-
-    def datagramReceived(self, data, (host, port)):
-        data = data.strip()
-        for line in data.splitlines():
-            key, val, type = line.split()
-            stat = self.getStat(type, key)
-            if stat:
-                stat.update(val)
 
     def getStat(self, type, key):
         full_key = "%s_%s" % (type, key)
@@ -103,16 +136,6 @@ class ReflexProtocol(protocol.DatagramProtocol):
                 return None
 
         return self.stats[full_key]
-
-    def startProtocol(self):
-        protocol.DatagramProtocol.startProtocol(self)
-
-        # start data rollup task
-        self.rollup_task = task.LoopingCall(self.rollupData)
-        self.rollup_task.start(60, now=False)
-
-    def stopProtocol(self):
-        protocol.DatagramProtocol.stopProtocol(self)
 
     def rollupData(self):
         for stat in self.stats:
