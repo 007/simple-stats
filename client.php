b600d78acc6609f4938efb617b765baea132e62a
scripting <scripting@f4a64cd6-bb93-48a9-bb03-caeeccae09ce>
2010-10-27 22:53:39 +0000

    'load testing' in client, parameterized calls, remove old .py reference client
    git-svn-id: https://murdock.reputationdefender.com/sourcecode/engineering/logserver/stats@245 f4a64cd6-bb93-48a9-bb03-caeeccae09ce

--- client.php
+++ client.php
@@ -1,19 +1,19 @@
 <?php
 function logStat($statKey, $statVal = 1, $statType = 'SUM') {
 
+    // TODO: parameterize this via configs or something
     $host_server = '127.0.0.1';
     $host_port = 13023;
 
-    // 10 second connect timeout
-    $conn = fsockopen('udp://' . $host_server, $host_port, $errno, $errstr, 10);
+    // 5 second connect timeout
+    $conn = fsockopen('udp://' . $host_server, $host_port, $errno, $errstr, 5);
     if (!$conn) {
-        // socket open error
-        // since we're using UDP, this should never happen;
+        // socket open error - since we're using UDP, this should never happen
         // UDP doesn't "open" a socket connection until data is written, it just allocates it
         return;
     }
-    // 10 second sockwrite timeout
-    stream_set_timeout($conn, 10);
+    // 5 second sockwrite timeout
+    stream_set_timeout($conn, 5);
     // strip tabs from key, any other character is valid
     $data = $statType . "\t" . str_replace("\t", "_", trim($statKey)) . "\t" . $statVal . "\n";
     fwrite($conn, $data);
@@ -21,13 +21,19 @@ function logStat($statKey, $statVal = 1, $statType = 'SUM') {
 }
 
 
-$prodArray = array("product1", "product2");
-logStat("products_sold", count($prodArray));
-foreach($prodArray as $key=>$val) {
-    logStat("product_" . $val, 1);
+// for load testing:
+$indexArray = array();
+for($i = 0; $i < 100; $i++) $indexArray[$i] = 0;
+$loopCount = 1000;
+//mt_srand(42);
+for ($i = 0; $i < $loopCount; $i++) {
+    $index = mt_rand(0, 99);
+    $val = mt_rand(1,10);
+    $indexArray[$index] += $val;
+    logStat('test_rrd_' . $index, $val);
 }
-logStat("key");
-logStat("key2");
-logStat("key3", 10);
 
+$total = 0;
+for($i = 0; $i < 100; $i++) $total += $indexArray[$i];
+echo "Dumped $total values\n";
 
