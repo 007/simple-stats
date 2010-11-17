d64215e7f0d4d2f5b4872dcc36c14035d5388837
Ryan_Moore <Ryan_Moore@f4a64cd6-bb93-48a9-bb03-caeeccae09ce>
2010-11-17 19:12:36 +0000

    more staticesque version of logstat, includes sync methods for multithreading and constructor for params
    git-svn-id: https://murdock.reputationdefender.com/sourcecode/engineering/logserver/stats@290 f4a64cd6-bb93-48a9-bb03-caeeccae09ce

--- LogStat.java
+++ LogStat.java
@@ -1,8 +1,11 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.lang.RuntimeException;
 
-class LogStat {
+final class LogStat {
+    private static LogStat instance = null;
+
     // for testing:
     // private static String server_host = "10.40.40.101";
     private static String server_host = "127.0.0.1";
@@ -13,17 +16,40 @@ class LogStat {
     private static DatagramSocket ds = null;
     private static InetSocketAddress sock_host = null;
 
-    static {
-        try {
-            if (buffer == null) { buffer = new StringBuffer(1024); }
-            if (ds == null) { ds = new DatagramSocket(); }
-            if (sock_host == null) { sock_host = new InetSocketAddress(server_host, server_port); }
-        } catch (Exception e) {
-        }
+    private LogStat() {
+        this(server_host, server_port);
     }
 
-    // example: LogStat.stat("key", 1, "SUM");
-    public synchronized static void stat(String key, int value, String type) {
+    private LogStat(String host, int port) {
+        server_host = host;
+        server_port = port;
+        try {
+            if (ds == null) { ds = new DatagramSocket(); }
+            if (buffer == null) { buffer = new StringBuffer(1024); }
+            if (sock_host == null) { sock_host = new InetSocketAddress(server_host, server_port); }
+        } catch (Exception e) { }
+    }
+
+    public static LogStat getInstance(String host, int port) throws RuntimeException {
+        if (instance == null) {
+            instance = new LogStat(host, port);
+        } else {
+            // if we're initializing for a second time with host/port, throw exception
+            throw new RuntimeException("LogStat already initialized");
+            // then keep going
+        }
+        return instance;
+    }
+
+    public static LogStat getInstance() {
+        if (instance == null) {
+            instance = new LogStat();
+        }
+        return instance;
+    }
+
+    // example: stat.log("key", 1, "SUM");
+    public static void log(String key, int value, String type) {
         try {
             buffer.setLength(0);
             buffer.append(type);
@@ -36,10 +62,13 @@ class LogStat {
             byte [] output = buffer.toString().getBytes();
             DatagramPacket packet = new DatagramPacket(output, output.length, sock_host);
             ds.send(packet);
-        } catch (Exception e) {
-        }
+        } catch (Exception e) { }
     }
-    public static void stat(String key, int value) { LogStat.stat(key, value, "SUM"); }
-    public static void stat(String key) { LogStat.stat(key, 1, "SUM"); }
+    public static void log(String key, int value) { LogStat.getInstance().log(key, value, "SUM"); }
+    public static void log(String key) { LogStat.getInstance().log(key, 1, "SUM"); }
+
+    public synchronized static void log_s(String key, int value, String type) { LogStat.getInstance().log(key, value, type); }
+    public synchronized static void log_s(String key, int value) { LogStat.getInstance().log(key, value); }
+    public synchronized static void log_s(String key) { LogStat.getInstance().log(key); }
 } 
 
