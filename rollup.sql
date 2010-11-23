239e94c37ad3ac4a92489c22a7611fa4ba19167a
Ryan_Moore <Ryan_Moore@f4a64cd6-bb93-48a9-bb03-caeeccae09ce>
2010-11-23 22:02:59 +0000

    rollup based on new table structure
    git-svn-id: https://murdock.reputationdefender.com/sourcecode/engineering/logserver/stats@313 f4a64cd6-bb93-48a9-bb03-caeeccae09ce

--- rollup.sql
+++ rollup.sql
@@ -15,10 +15,10 @@ DELETE FROM raw WHERE func = 'SUM' AND data = 0;
 -- minute summary grouping
 SET @atomic_now = TRUNC_TO_MINUTE(@atomic_now);
 START TRANSACTION;
-REPLACE minute_summary SELECT TRUNC_TO_MINUTE(ts) t, func, stat, SUM(data) FROM raw WHERE func = 'SUM' AND ts < @atomic_now GROUP BY t, stat;
-REPLACE minute_summary SELECT TRUNC_TO_MINUTE(ts) t, func, stat, AVG(data) FROM raw WHERE func = 'AVG' AND ts < @atomic_now GROUP BY t, stat;
-REPLACE minute_summary SELECT TRUNC_TO_MINUTE(ts) t, func, stat, MIN(data) FROM raw WHERE func = 'MIN' AND ts < @atomic_now GROUP BY t, stat;
-REPLACE minute_summary SELECT TRUNC_TO_MINUTE(ts) t, func, stat, MAX(data) FROM raw WHERE func = 'MAX' AND ts < @atomic_now GROUP BY t, stat;
+REPLACE minute_summary SELECT TRUNC_TO_MINUTE(ts) t, TRUNC_TO_HOUR(ts) ts_h, TRUNC_TO_DAY(ts) ts_d, func, stat, SUM(data) FROM raw WHERE func = 'SUM' AND ts < @atomic_now GROUP BY t, stat;
+REPLACE minute_summary SELECT TRUNC_TO_MINUTE(ts) t, TRUNC_TO_HOUR(ts) ts_h, TRUNC_TO_DAY(ts) ts_d, func, stat, AVG(data) FROM raw WHERE func = 'AVG' AND ts < @atomic_now GROUP BY t, stat;
+REPLACE minute_summary SELECT TRUNC_TO_MINUTE(ts) t, TRUNC_TO_HOUR(ts) ts_h, TRUNC_TO_DAY(ts) ts_d, func, stat, MIN(data) FROM raw WHERE func = 'MIN' AND ts < @atomic_now GROUP BY t, stat;
+REPLACE minute_summary SELECT TRUNC_TO_MINUTE(ts) t, TRUNC_TO_HOUR(ts) ts_h, TRUNC_TO_DAY(ts) ts_d, func, stat, MAX(data) FROM raw WHERE func = 'MAX' AND ts < @atomic_now GROUP BY t, stat;
 -- clear out raw table so we don't double-count anything
 DELETE FROM raw WHERE ts < @atomic_now;
 COMMIT;
@@ -26,19 +26,19 @@ COMMIT;
 -- hour summary grouping
 SET @atomic_now = TRUNC_TO_HOUR(@atomic_now);
 START TRANSACTION;
-REPLACE hour_summary SELECT TRUNC_TO_HOUR(ts) t, func, stat, SUM(data) FROM minute_summary WHERE func = 'SUM' AND ts < @atomic_now GROUP BY t, stat;
-REPLACE hour_summary SELECT TRUNC_TO_HOUR(ts) t, func, stat, AVG(data) FROM minute_summary WHERE func = 'AVG' AND ts < @atomic_now GROUP BY t, stat;
-REPLACE hour_summary SELECT TRUNC_TO_HOUR(ts) t, func, stat, MIN(data) FROM minute_summary WHERE func = 'MIN' AND ts < @atomic_now GROUP BY t, stat;
-REPLACE hour_summary SELECT TRUNC_TO_HOUR(ts) t, func, stat, MAX(data) FROM minute_summary WHERE func = 'MAX' AND ts < @atomic_now GROUP BY t, stat;
+REPLACE hour_summary SELECT ts_h, ts_d, func, stat, SUM(data) FROM minute_summary WHERE func = 'SUM' AND ts < @atomic_now GROUP BY ts_h, stat;
+REPLACE hour_summary SELECT ts_h, ts_d, func, stat, AVG(data) FROM minute_summary WHERE func = 'AVG' AND ts < @atomic_now GROUP BY ts_h, stat;
+REPLACE hour_summary SELECT ts_h, ts_d, func, stat, MIN(data) FROM minute_summary WHERE func = 'MIN' AND ts < @atomic_now GROUP BY ts_h, stat;
+REPLACE hour_summary SELECT ts_h, ts_d, func, stat, MAX(data) FROM minute_summary WHERE func = 'MAX' AND ts < @atomic_now GROUP BY ts_h, stat;
 COMMIT;
 
 -- day summary grouping
 SET @atomic_now = TRUNC_TO_DAY(@atomic_now);
 START TRANSACTION;
-REPLACE day_summary SELECT TRUNC_TO_DAY(ts) t, func, stat, SUM(data) FROM hour_summary WHERE func = 'SUM' AND ts < @atomic_now GROUP BY t, stat;
-REPLACE day_summary SELECT TRUNC_TO_DAY(ts) t, func, stat, AVG(data) FROM hour_summary WHERE func = 'AVG' AND ts < @atomic_now GROUP BY t, stat;
-REPLACE day_summary SELECT TRUNC_TO_DAY(ts) t, func, stat, MIN(data) FROM hour_summary WHERE func = 'MIN' AND ts < @atomic_now GROUP BY t, stat;
-REPLACE day_summary SELECT TRUNC_TO_DAY(ts) t, func, stat, MAX(data) FROM hour_summary WHERE func = 'MAX' AND ts < @atomic_now GROUP BY t, stat;
+REPLACE day_summary SELECT ts_d, func, stat, SUM(data) FROM hour_summary WHERE func = 'SUM' AND ts < @atomic_now GROUP BY ts_d, stat;
+REPLACE day_summary SELECT ts_d, func, stat, AVG(data) FROM hour_summary WHERE func = 'AVG' AND ts < @atomic_now GROUP BY ts_d, stat;
+REPLACE day_summary SELECT ts_d, func, stat, MIN(data) FROM hour_summary WHERE func = 'MIN' AND ts < @atomic_now GROUP BY ts_d, stat;
+REPLACE day_summary SELECT ts_d, func, stat, MAX(data) FROM hour_summary WHERE func = 'MAX' AND ts < @atomic_now GROUP BY ts_d, stat;
 COMMIT;
 
 -- cleanup old entries in minute/hour table
